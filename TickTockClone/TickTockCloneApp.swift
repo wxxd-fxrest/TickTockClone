88597e241952b41306360ed0cf8c2b92b0559421
wxxd-fxrest <fxrest704@gmail.com>
2024-02-27 14:07:31 +0900

    Initial Commit

---
+++ TickTockClone/TickTockCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickTockCloneApp.swift
+//  TickTockClone
+//
+//  Created by 밀가루 on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct TickTockCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
